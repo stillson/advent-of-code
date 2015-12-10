f7c4eb58d6beca418799962d998bcd8e77ff1487
Alice Maz <alice@alicemaz.com>
2015-12-10 18:27:00 -0500

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 
 struct Advent;
 
 impl Advent {
-    fn d1a(&self) {
-        let input = include_bytes!("../data/d1a");
+    fn d1(&self) {
+        let input = include_bytes!("../data/d1");
         let mut floor = 0;
         let mut basement = 0;
 
@@ -22,6 +24,36 @@ impl Advent {
 
         println!("floor {}\nbasement {}", floor, basement);
     }
+
+    fn d2(&self) {
+        let f = File::open("data/d2").unwrap();
+        let f = BufReader::new(f);
+
+        let mut paper = 0;
+        let mut ribbon = 0;
+
+        for line in f.lines() {
+            let line = line.unwrap();
+
+            //zzz can't [l,w,h] yet sadly 
+            let lwh = line.split("x")
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            let (a, b, c) = (lwh[0] * lwh[1], lwh[0] * lwh[2], lwh[1] * lwh[2]);
+            paper += 2 * (a + b + c) + least(a, b, c);
+
+            let (p1, p2, p3) = (2 * (lwh[0] + lwh[1]), 2 * (lwh[0] + lwh[2]), 2 * (lwh[1] + lwh[2]));
+            ribbon += least(p1, p2, p3) + lwh[0] * lwh[1] * lwh[2];
+
+        }
+        
+        println!("paper: {}\nribbon: {}", paper, ribbon);
+    }
+}
+
+fn least(x: i32, y: i32, z: i32) -> i32 {
+    if x < y && x < z {x} else if y < z {y} else {z}
 }
 
 fn main() {
@@ -30,7 +62,8 @@ fn main() {
     if args.len() > 1 {
         //this is silly but whatevs
         match &*args[1] {
-            "d1a" => Advent.d1a(),
+            "d1" => Advent.d1(),
+            "d2" => Advent.d2(),
             _ => println!("something happened")
         }
     }
